a1b0e1ae75d496b563fe4d70b30476aae5fa5398
jea-jun <seghuo22@gmail.com>
2024-07-15 21:52:46 +0900

    Delete nave.py

--- nave.py
+++
@@ -1,8 +0,0 @@
-
-#list6 = list(map(lambda x :'0' if x == 0 ('홀수' if x %2 == 1 else '짝수'). range(11)))
-#print(list6)
-
-list6 = list(map(lambda x: '0' if x == 0 else ('홀수' if x % 2 == 1 else '짝수'), range(11)))
-
-# 결과 출력
-print(list6)
